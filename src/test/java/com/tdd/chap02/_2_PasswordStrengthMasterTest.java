ffb684580a8a01ca1315a9469e3bed154b9fbae6
mskwon <mskwon@tidesquare.com>
2023-01-27 22:32:06 +0900

    add: TDD 예: 암호 검사기

---
+++ src/test/java/com/tdd/chap02/_2_PasswordStrengthMasterTest.java
@@ -0,0 +1,23 @@
+package com.tdd.chap02;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 암호 검사기
+ *
+ * 규칙
+ * - 길이가 8글자 이상
+ * - 0부터 9사이의 숫자를 포함
+ * - 대문자 포함
+ * - 세 규칙을 모두 충족하면 암호는 강함이다
+ * - 2개의 규칙을 충족하면 암호는 보통이다
+ * - 1개 이하의 규칙을 충족하면 암호는 약함이다
+ */
+public class _2_PasswordStrengthMasterTest {
+
+    @Test
+    void name() {
+
+    }
+}
