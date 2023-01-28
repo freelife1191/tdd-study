7f16374bffc812128ad0b65baa4bc68bb2401d5e
mskwon <mskwon@tidesquare.com>
2023-01-28 13:39:05 +0900

    refactor: 코드 정리: meter() 메서드 리팩토링

--- src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
@@ -3,19 +3,12 @@ package com.tdd.chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainningNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
-        if (lengthEnough && !containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if (!lengthEnough && containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if (!lengthEnough && !containsNum && containsUpp)
-            return PasswordStrength.WEAK;
-        if (!lengthEnough)
-            return PasswordStrength.NORMAL;
-        if (!containsNum) return PasswordStrength.NORMAL;
-        if (!containsUpp) return PasswordStrength.NORMAL;
+        int metConunts = 0;
+        if (s.length() >= 8) metConunts++;
+        if (meetsContainningNumberCriteria(s)) metConunts++;
+        if (meetsContainingUppercaseCriteria(s)) metConunts++;
+        if (metConunts == 1) return PasswordStrength.WEAK;
+        if (metConunts == 2) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 
