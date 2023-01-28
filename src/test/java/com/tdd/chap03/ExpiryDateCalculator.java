c0abed7a44ccccb4f020f0438b13bcb854bd24be
mskwon <mskwon@tidesquare.com>
2023-01-28 23:30:23 +0900

    (refactoring) 상수를 변수로 바꾸는 리팩토링

--- src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = 1;
         if (payData.getFirstBillingDate() != null) {
             // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
