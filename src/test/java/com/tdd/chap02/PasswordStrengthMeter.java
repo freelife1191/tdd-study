2879a5a6cc000b505efbfbd26ddd054b117d66fd
mskwon <mskwon@tidesquare.com>
2023-01-28 12:30:50 +0900

    refactor: 코드 가독성을 위해 숫자 포함 여부를 확인하는 코드를 리팩토링

--- src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
@@ -5,14 +5,17 @@ public class PasswordStrengthMeter {
         if (s.length() < 8) {
             return PasswordStrength.NORMAL;
         }
-        boolean containsNum = false;
-        for (char ch : s.toCharArray()) {
-            if (ch >= '0' && ch <= '9') {
-                containsNum = true;
-                break;
-            }
-        }
+        boolean containsNum = meetsContainningNumberCriteria(s);
         if (!containsNum) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean meetsContainningNumberCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 }
