1e0c4953f86d9d906d9b94d0d9b6b78b830c9266
mskwon <mskwon@tidesquare.com>
2023-01-28 23:54:59 +0900

    (refactoring) 첫 납부일의 일자를 구하는 코드의 중복 제거

--- src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
@@ -9,13 +9,14 @@ public class ExpiryDateCalculator {
         if (payData.getFirstBillingDate() != null) {
             // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
                 // 후보 만료일이 포함된 달의 마지막 날 < 첫 납부일의 일자 추가
                 int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                if (dayLenOfCandiMon < dayOfFirstBilling) {
                     return candidateExp.withDayOfMonth(dayLenOfCandiMon);
                 }
-                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+                return candidateExp.withDayOfMonth(dayOfFirstBilling);
             }
         }
         return payData.getBillingDate().plusMonths(addedMonths);
