32a2e1bfadb26929b92e72107496a7418acacacd
mskwon <mskwon@tidesquare.com>
2023-01-28 23:49:12 +0900

    (pass) 후보 만료일이 포함된 달의 일자 길이에 따른 만료일 계산 로직 추가

--- src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
@@ -10,6 +10,10 @@ public class ExpiryDateCalculator {
             // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                // 후보 만료일이 포함된 달의 마지막 날 < 첫 납부일의 일자 추가
+                if (YearMonth.from(candidateExp).lengthOfMonth()<payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+                }
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
