e06df225795ef7c86844b172baf6556f60ffa697
mskwon <mskwon@tidesquare.com>
2023-01-29 00:07:47 +0900

    (refactoring) 보조 메서드를 만들어 코드 가독성을 개선

--- src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/tdd/chap03/ExpiryDateCalculator.java
@@ -16,10 +16,10 @@ public class ExpiryDateCalculator {
     private static LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
         // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
         LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+        if (isSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
             // 후보 만료일이 포함된 달의 마지막 날 < 첫 납부일의 일자 추가
-            int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
             if (dayLenOfCandiMon < dayOfFirstBilling) {
                 return candidateExp.withDayOfMonth(dayLenOfCandiMon);
             }
@@ -27,5 +27,13 @@ public class ExpiryDateCalculator {
         } else {
             return candidateExp;
         }
+    }
+
+    private static boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() != date2.getDayOfMonth();
+    }
+
+    private static int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
