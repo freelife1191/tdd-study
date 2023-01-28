235f362f449a7657544c6b7188e7677741b8ba9d
mskwon <mskwon@tidesquare.com>
2023-01-28 12:49:44 +0900

    pass: 입력이 빈 문자열인 경우에 대한 테스트 추가

--- src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/tdd/chap02/PasswordStrengthMeter.java
@@ -2,7 +2,7 @@ package com.tdd.chap02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
-        if (s == null) return PasswordStrength.INVALID;
+        if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
         if (s.length() < 8) {
             return PasswordStrength.NORMAL;
         }
