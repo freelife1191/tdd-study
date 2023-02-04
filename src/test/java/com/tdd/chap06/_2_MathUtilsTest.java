5149e5727bb45f102867ca7d8ee52fe63eee4242
mskwon <mskwon@tidesquare.com>
2023-02-04 23:26:29 +0900

    add givenNoFile method

--- src/test/java/com/tdd/chap06/_2_MathUtilsTest.java
+++ src/test/java/com/tdd/chap06/_2_MathUtilsTest.java
@@ -15,10 +15,26 @@ public class _2_MathUtilsTest {
     @DisplayName("파일이 존재하지 않으면 IllegalArgumentException 발생")
     @Test
     void noDataFile_Then_Exception() {
+        // 명시적으로 파일이 없는 상황 만들기
+        givenNoFile("badpath.txt");
+
         File dataFile = new File("badpath.txt");
         assertThrows(IllegalArgumentException.class,
             () -> MathUtils.sum(dataFile)
         );
+    }
+
+    /**
+     * 해당 경로에 파일이 존재하는지 검사해서 존재할 경우 해당 파일을 삭제
+     * @param path
+     */
+    private void givenNoFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            boolean deleted = file.delete();
+            if (!deleted)
+                throw new RuntimeException("fail givenNoFile: " + path);
+        }
     }
 
 }
