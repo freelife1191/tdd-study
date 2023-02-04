61ba458a7e0c64764019ec1fa4bd1d8ac1cdf062
mskwon <mskwon@tidesquare.com>
2023-02-05 00:22:15 +0900

    (test pass) dataFileSumTest2

--- src/test/java/com/tdd/chap06/_2_MathUtilsTest.java
+++ src/test/java/com/tdd/chap06/_2_MathUtilsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,6 +23,33 @@ public class _2_MathUtilsTest {
         File dataFile = new File("src/test/resources/datafile.txt");
         long sum = MathUtils.sum(dataFile);
         assertEquals(10L, sum);
+    }
+
+    @Test
+    void dataFileSumTest2() {
+        // 테스트 코드 안에 필요한 것이 다 있음
+        givenDataFile("out/datafile.txt", "1","2","3","4");
+        File dataFile = new File("src/test/resources/datafile.txt");
+        long sum = MathUtils.sum(dataFile);
+        assertEquals(10L, sum);
+    }
+
+    /**
+     * 파일을 미리 만들지 않고 테스트 코드에서 상황에 맞는 파일을 생성하는 방법
+     * @param path
+     * @param lines
+     */
+    private void givenDataFile(String path, String... lines) {
+        try {
+            Path dataPath = Paths.get(path);
+            System.out.println(dataPath);
+            if (Files.exists(dataPath)) {
+                Files.delete(dataPath);
+            }
+            Files.write(dataPath, Arrays.asList(lines));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DisplayName("파일이 존재하지 않으면 IllegalArgumentException 발생")
