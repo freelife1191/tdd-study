96a4000ff06819cd4061de3fa1a8a1ea627b028a
mskwon <mskwon@tidesquare.com>
2023-02-05 15:04:35 +0900

    (test fail) AutoDebitRegisterTest

---
+++ src/test/java/com/tdd/chap07/autodebit/AutoDebitRegisterTest.java
@@ -0,0 +1,38 @@
+package com.tdd.chap07.autodebit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.tdd.chap07.autodebit.CardValidity.THEFT;
+import static com.tdd.chap07.autodebit.CardValidity.VALID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 살제 구현을 사용하는 테스트 코드
+ */
+public class AutoDebitRegisterTest {
+    private AutoDebitRegister register;
+
+    @BeforeEach
+    void setUp() {
+        CardNumberValidator validator = new CardNumberValidator();
+        AutoDebitInfoRepository repository = new JpaAutoDebitInfoRepository();
+        register = new AutoDebitRegister(validator, repository);
+    }
+
+    @Test
+    void validCard() {
+        // 업체에서 받은 테스트용 유효한 카드 번호 사용
+        AutoDebitReq req = new AutoDebitReq("user1", "1234123412341234");
+        RegisterResult result = this.register.register(req);
+        assertEquals(VALID, result.getValidity());
+    }
+
+    @Test
+    void theftCard() {
+        // 업체에서 받은 도난 테스트용 카드 번호 사용
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+        RegisterResult result = this.register.register(req);
+        assertEquals(THEFT, result.getValidity());
+    }
+}
